f9f85cda1b613f7a8ffd75f976941e2d5892d0f9
code-with-nikumit <159416208+code-with-nikumit@users.noreply.github.com>
2024-02-29 14:25:45 +0530

    Add files via upload

---
+++ school/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.multiple-items').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+
+
+    });
+    $('.owl-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+    });
+    $('.owl-theme').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+    });
+
+});
